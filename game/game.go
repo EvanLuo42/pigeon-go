ddf4da9e0071b36855239d00d995325a4939d2a3
Evan Luo <knacks.insects_0o@icloud.com>
2024-06-13 13:15:52 +0800

    feat: Add deltaTime

--- game/game.go
+++ game/game.go
@@ -1,19 +1,43 @@
 package game
 
 import (
+	"fmt"
 	"github.com/asynkron/protoactor-go/actor"
+	"math/rand"
 	"pigeon/pb/movement"
 	"strconv"
+	"time"
 )
 
 type (
 	GameActor struct {
 		Server *actor.PID
 	}
+
+	Update struct {
+		deltaTime float64
+	}
 )
 
 func (g *GameActor) Receive(c actor.Context) {
 	switch msg := c.Message().(type) {
+	case *actor.Started:
+		go func() {
+			ticker := time.NewTicker(time.Second)
+			defer ticker.Stop()
+			deltaTime := 0.0
+			for {
+				startTime := time.Now()
+				select {
+				case <-ticker.C:
+					c.RequestFuture(c.Self(), Update{deltaTime: deltaTime}, 5*time.Second).Wait()
+					endTime := time.Now()
+					deltaTime = endTime.Sub(startTime).Seconds()
+				}
+			}
+		}()
+	case Update:
+		g.Update(msg, c)
 	case *movement.Test:
 		g.Test(msg, c)
 	}
@@ -21,4 +45,11 @@ func (g *GameActor) Receive(c actor.Context) {
 
 func (g *GameActor) Test(msg *movement.Test, c actor.Context) {
 	c.Logger().Info(strconv.Itoa(int(msg.Test)))
+}
+
+func (g *GameActor) Update(msg Update, c actor.Context) {
+	fmt.Println(msg.deltaTime)
+	num := rand.Intn(4)
+	time.Sleep(time.Duration(num) * time.Second)
+	c.Respond(1)
 }
